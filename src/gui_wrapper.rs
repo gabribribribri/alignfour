e74a833cf30f5823c4e64660f004f5d6631386c5
gabribribribri <fuelanemo149@gmail.com>
2024-05-10 16:12:58 +0200

    added cool outline

--- src/gui_wrapper.rs
+++ src/gui_wrapper.rs
@@ -1,7 +1,6 @@
 use crate::align_four_engine::{AlignFourEngine, Team};
 use sfml::{
     graphics::{CircleShape, Color, Rect, RenderTarget, RenderWindow, Shape, Transformable, View},
-    system::Vector2,
     window::{mouse::Button, Event, Key, Style},
 };
 use std::{
@@ -9,8 +8,8 @@ use std::{
     time::{Duration, Instant},
 };
 
-const BLUE_TEAM: Color = Color::rgb(22, 130, 224);
-const RED_TEAM: Color = Color::rgb(150, 29, 47);
+const BLUE_COLOR_LIGHT: Color = Color::rgb(22, 130, 224);
+const RED_COLOR_LIGHT: Color = Color::rgb(150, 29, 47);
 const FPS_LIMIT: u64 = 120;
 const FRAME_TIME: Duration = Duration::from_millis(1000 / FPS_LIMIT);
 
@@ -23,9 +22,10 @@ pub struct GUIWrapper<'a> {
 impl<'a> GUIWrapper<'a> {
     //Constructors
     pub fn default() -> Self {
+        let circles = vec![CircleShape::new(50.0, 30); 6 * 7];
         Self {
             engine: AlignFourEngine::default(),
-            circles: vec![CircleShape::new(50.0, 30); 6 * 7],
+            circles,
             window: RenderWindow::new(
                 (1280, 720),
                 "alignfour",
@@ -42,7 +42,9 @@ impl<'a> GUIWrapper<'a> {
             if self.handle_win() {
                 self.window.close()
             }
+            self.handle_over();
             self.update_circles();
+            //draw to screen
             self.window.clear(Color::rgb(96, 96, 96));
             self.draw_circles();
             self.window.display();
@@ -73,8 +75,8 @@ impl<'a> GUIWrapper<'a> {
                 Event::MouseButtonPressed {
                     button: Button::Left,
                     x,
-                    y,
-                } => self.handle_click(x, y),
+                    ..
+                } => self.handle_click(x),
                 _ => (),
             }
         }
@@ -83,7 +85,7 @@ impl<'a> GUIWrapper<'a> {
     fn handle_win(&self) -> bool {
         match self.engine.check_win() {
             Some(Team::Red | Team::Blue) => {
-                println!("Les {} ont gagnés !!!", self.turn_color());
+                println!("Les {} ont gagnés !!!", self.turn_color_string());
                 true
             }
             Some(Team::Nothing) => {
@@ -94,18 +96,35 @@ impl<'a> GUIWrapper<'a> {
         }
     }
 
-    fn handle_click(&mut self, x: i32, y: i32) {
+    fn handle_click(&mut self, x: i32) {
         for gx in 0..self.engine.width() {
-            for gy in 0..self.engine.height() {
-                if self
-                    .at(gx, gy)
-                    .global_bounds()
-                    .contains(Vector2::new(x as f32, y as f32))
-                {
-                    match self.engine.play_at(gx) {
-                        Ok(_) => self.engine.switch_turns(),
-                        Err(_) => (),
-                    }
+            let col = self.at(gx, 0).global_bounds();
+            let col_left = col.left;
+            let col_right = col.left + col.width;
+            if col_left < x as f32 && col_right > x as f32 {
+                match self.engine.play_at(gx) {
+                    Ok(_) => self.engine.switch_turns(),
+                    Err(_) => (),
+                }
+            }
+        }
+    }
+
+    fn handle_over(&mut self) {
+        let x = self.window.mouse_position().x;
+        for gx in 0..self.engine.width() {
+            let col = self.at(gx, 0).global_bounds();
+            let col_left = col.left;
+            let col_right = col.left + col.width;
+            if col_left < x as f32 && col_right > x as f32 {
+                for gy in 0..self.engine.height() {
+                    let turn_color = self.turn_color_light();
+                    self.at_mut(gx, gy).set_outline_thickness(-5.0);
+                    self.at_mut(gx, gy).set_outline_color(turn_color);
+                }
+            } else {
+                for gy in 0..self.engine.height() {
+                    self.at_mut(gx, gy).set_outline_thickness(0.0);
                 }
             }
         }
@@ -141,18 +160,26 @@ impl<'a> GUIWrapper<'a> {
     fn update_circles(&mut self) {
         for (i, cell) in self.engine.grid().iter().enumerate() {
             self.circles[i].set_fill_color(match cell {
-                Team::Blue => BLUE_TEAM,
-                Team::Red => RED_TEAM,
+                Team::Blue => BLUE_COLOR_LIGHT,
+                Team::Red => RED_COLOR_LIGHT,
                 Team::Nothing => Color::rgb(64, 64, 64),
             });
         }
     }
 
-    fn turn_color(&self) -> String {
+    fn turn_color_string(&self) -> String {
         if self.engine.turn() != Team::Red {
             String::from("rouges")
         } else {
             String::from("bleus")
+        }
+    }
+
+    fn turn_color_light(&self) -> Color {
+        if self.engine.turn() == Team::Red {
+            RED_COLOR_LIGHT
+        } else {
+            BLUE_COLOR_LIGHT
         }
     }
 
@@ -161,6 +188,7 @@ impl<'a> GUIWrapper<'a> {
         &self.circles[y * self.engine.width() + x]
     }
 
+    // j'ai spam toutes les combinaisons de lifetime jusqu'à ce que ça fonctionne mais maintenant j'ai compris
     fn at_mut<'b>(&'b mut self, x: usize, y: usize) -> &'b mut CircleShape<'a> {
         &mut self.circles[y * self.engine.width() + x]
     }
